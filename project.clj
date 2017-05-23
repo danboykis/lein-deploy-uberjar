aebc15ac2b388a771d11cdbe3649ee58c841b6ea
Dan Boykis <danboykis@gmail.com>
2017-05-23 09:53:38 -0400

    deploy both jar and uberjar
    zipping up uberjar before sending it

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject camechis/deploy-uberjar "0.3.0"
+(defproject com.danboykis/deploy-uberjar "0.3.2-SNAPSHOT"
   :description "basic lein plugin to deploy an uberjar"
   :url "https://github.com/camechis/lein-deploy-uberjar"
   :license {:name "Apache License 2.0"
--- src/leiningen/deploy_uberjar.clj
+++ src/leiningen/deploy_uberjar.clj
@@ -1,11 +1,17 @@
 (ns leiningen.deploy-uberjar
   (:require [leiningen.uberjar :as luber]
+            [leiningen.jar :as jar]
+            [clojure.java.io :as io]
+            [leiningen.core.main :as main :refer [info]]
             [leiningen.pom :as lpom]
             [leiningen.deploy :as ldeploy]
-            [clojure.java.io :as io]))
+            [clojure.pprint :refer [pprint]]
+            [clojure.string :as string]
+            [cemerick.pomegranate.aether :as aether])
+  (:import [java.util.zip ZipEntry ZipOutputStream]))
 
 (defn exit [message]
-  (leiningen.core.main/info message)
+  (info message)
   (System/exit 1))
 
 (defn repo-target?
@@ -17,23 +23,59 @@
   (when-not (repo-target? project repo)
     (exit (format "Could not find the target repository: %s." repo))))
 
-
 (defn get-group-name [project]
   (format "%s/%s" (:group project) (:name project)))
 
-(defn get-uberjar-path [project]
-  (format (format "%s/%s" (:target-path project)(:uberjar-name project))
-          (:version project)))
+(defn insert [v pos item]
+  (into [] (apply conj (subvec v 0 pos) item (subvec v pos))))
+
+(defn copy-file [orig-path orig-file dest-path dest-file]
+  (io/copy (io/file (format "%s/%s" orig-path orig-file))
+           (io/file (format "%s/%s" dest-path dest-file)))
+  (format "%s/%s" dest-path dest-file))
+
+(defn zip
+  "Writes the contents of input to output, compressed.
+  input: something which can be copied from by io/copy.
+  output: something which can be opend by io/output-stream.
+  The bytes written to the resulting stream will be gzip compressed."
+  [input output & opts]
+  (with-open [zos (-> output io/output-stream ZipOutputStream.)]
+    (.putNextEntry zos (ZipEntry. (-> input java.io.File. .getName)))
+    (apply io/copy (io/file input) zos opts))
+    output)
+
+(defn get-uberjar-path [{:keys [version target-path] {{:keys [uberjar-name]} :uberjar} :profiles :as project}]
+  (let [uj-file (string/join "-" (-> uberjar-name
+                                     (string/split #"-")
+                                     (insert 1 version)))]
+    (copy-file target-path uberjar-name target-path uj-file)))
+
+(defn find-regular-jar [{:keys [target-path name version]}]
+  (let [p (re-pattern (str name "-" version ".jar"))]
+    (first
+      (filter #(re-matches p %) (seq (.list (io/file target-path)))))))
 
 (defn uberjar? [uberjar]
   (.exists (io/as-file uberjar)))
 
+(defn files-for [project repo uberjar-gz]
+  (merge (jar/jar project)
+         {[:extension "pom"] (lpom/pom project)}
+         {[:extension "zip"] uberjar-gz}))
+
 (defn deploy-uberjar
   "Deploy uberjar to the given repository"
-  [project & args]
-  (let [repo (first args)
-        uberjar (get-uberjar-path project)]
+  [{group :group target-path :target-path version :version :as project} & args]
+  (let [repo    (first args)
+        regular-jar (str target-path "/" (find-regular-jar project))
+        uberjar (-> project get-uberjar-path)
+        uberjar-gz (zip uberjar (str uberjar ".zip"))]
     (verify-repo project repo)
     (if (uberjar? uberjar)
-      (ldeploy/deploy project repo (get-group-name project) (:version project) uberjar (lpom/pom project))
+      (aether/deploy
+         :coordinates  [(symbol group (:name project)) version]
+         :artifact-map (files-for (assoc project :auto-clean false) repo uberjar-gz)
+         :transfer-listener :stdout
+         :repository   [(ldeploy/repo-for project repo)])
       (exit "uberjar does not exist, try lein uberjar first"))))
